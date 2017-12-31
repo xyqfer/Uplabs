df768330f772cc8a5c92061d012a83a5a5f635d3
xyqfer <xyqfer@gmail.com>
2017-12-31 10:48:15 +0800

    第一版完成
    - 支持浏览列表和查看大图

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,59 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    list: [],
+    offset: 0,
+    isLoading: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.loadData();
+  },
+
+  loadData: function() {
+    if (!this.data.isLoading) {
+      this.setData({
+        isLoading: true
+      });
+
+      wx.request({
+        url: `https://sy2bnjwp1a.leanapp.cn/api/v1/uplabs/all?offset=${this.data.offset}`,
+        success: (res) => {
+          this.setData({
+            list: this.data.list.concat(res.data),
+            offset: this.data.offset + 1,
+            isLoading: false
+          });
+        }
+      });
+    }
+  },
+
+  showPreview: function(event) {
+    const id = event.currentTarget.dataset.id;
+    const filterList = this.data.list.filter((item) => {
+      return item.id == id;
+    });
+
+    if (filterList.length > 0) {
+      const data = filterList[0];
+      let current = data.preview_url;
+      let urls = [current];
+
+      if (data.images.length > 0) {
+        data.images.forEach((img) => {
+          urls.push(img.urls.full);
+        });
+      }
+
+      wx.previewImage({
+        current: current,
+        urls: urls
+      });
+    }
   },
 
   /**
@@ -54,7 +99,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    this.loadData();
   },
 
   /**
@@ -62,7 +107,8 @@ Page({
    */
   onShareAppMessage: function () {
     return {
-      title: 'Uplabs 分享了一张图'
+      title: 'Uplabs',
+      imageUrl: './images/screenshot.png'
     };
   }
 })
